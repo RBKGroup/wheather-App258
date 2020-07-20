1150ab70b1af05e4ac85a1c144ac6fe0bf58272a
Ahmed Hilles <57227022+Ahmed116@users.noreply.github.com>
2020-07-20 23:47:08 +0300

    basic

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// fix me
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
