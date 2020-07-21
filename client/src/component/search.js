1b32339b4def16139afda8d2ed7c5baca598bb8b
Ahmed Hilles <57227022+Ahmed116@users.noreply.github.com>
2020-07-21 11:59:13 +0300

    headers

--- client/src/component/search.js
+++ client/src/component/search.js
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 //import "./style.css";
 //  import { query } from 'express';
-import axios from "axios";
+import axios from 'axios';
 const api = {
-  key: "21e8aec578e07d1343c0942cb7627fa1",
-  base: "https:api.openweathermap.org/data/2.5/",
+  key: '21e8aec578e07d1343c0942cb7627fa1',
+  base: 'https:api.openweathermap.org/data/2.5/',
 };
 
 function Weathers() {
   // var className = "Weathers";
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-//   const search = (evt) => {
-//     if (evt.key === "Enter") {
-//   axios.get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-//   .then((result) => {
-//     setWeather(result);
-//     setQuery("");
-//     console.log(result);
-//   });
-// }
-// }
+  //   const search = (evt) => {
+  //     if (evt.key === "Enter") {
+  //   axios.get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  //   .then((result) => {
+  //     setWeather(result);
+  //     setQuery("");
+  //     console.log(result);
+  //   });
+  // }
+  // }
   const search = (evt) => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    if (evt.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      })
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          setQuery("");
+          setQuery('');
           console.log(result);
         });
     }
@@ -36,28 +41,28 @@ function Weathers() {
 
   var dateBuilder = (d) => {
     var months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
     ];
 
     var days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
     ];
 
     var day = days[d.getDay()];
@@ -71,41 +76,41 @@ function Weathers() {
   return (
     <div
       className={
-        typeof weather.main != "undefined"
+        typeof weather.main != 'undefined'
           ? weather.main.temp > 16
-            ? "Weathers warm"
-            : "Weathers cold"
-          : "Weathers"
+            ? 'Weathers warm'
+            : 'Weathers cold'
+          : 'Weathers'
       }
     >
       <main>
-        <div className="search-box">
+        <div className='search-box'>
           <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
+            type='text'
+            className='search-bar'
+            placeholder='Search...'
             onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
 
-        {typeof weather.main != "undefined" ? (
+        {typeof weather.main != 'undefined' ? (
           <div>
-            <div className="location-box">
-              <div className="location">
+            <div className='location-box'>
+              <div className='location'>
                 {weather.name}, {weather.sys.country}
               </div>
-              <div className="date">{dateBuilder(new Date())}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
 
-              <div className="weather-box">
-                <div className="temp">{Math.round(weather.main.temp)}°C</div>
-                <div className="weather"> {weather.weather[0].main}</div>
+              <div className='weather-box'>
+                <div className='temp'>{Math.round(weather.main.temp)}°C</div>
+                <div className='weather'> {weather.weather[0].main}</div>
               </div>
             </div>
           </div>
         ) : (
-          ""
+          ''
         )}
       </main>
     </div>
