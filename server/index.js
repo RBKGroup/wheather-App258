093cab47b64f4258727a64eb61d2e1973f6eed1c
Ahmed Hilles <57227022+Ahmed116@users.noreply.github.com>
2020-07-21 11:50:10 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
+app.use(cors());
+
 const reg = require('../db/index');
 let AccountsModel = reg.AccountsModel;
 
